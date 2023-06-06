25a75cd780f83963d084739e3c3f2b43a09f9e44
Mike Chen <mikechen.dev@gmail.com>
2023-06-06 18:58:34 +0800

    feat: Update server.js for production

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import postsRoutes from './routes/postsRoutes.js';
 import spotifyRoutes from './routes/spotifyRoutes.js';
+import * as path from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -18,13 +19,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello api');
-});
-
-// app.get('/api/users', (req, res) => {
-//   res.json(demoProfiles);
-// });
 app.post('/api/login-spotify', (req, res) => {
   if (req.body.code) {
     const code = req.body.code;
@@ -45,8 +39,21 @@ app.post('/api/login-spotify', (req, res) => {
       .catch((err) => console.log(err));
   }
 });
+
 // app.use('/api/spotify', spotifyRoutes);
 app.use('/api/posts', postsRoutes);
+
+// Setting for production
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => res.send('API is running'));
+}
 
 app.use(notFound);
 app.use(errorHandler);
